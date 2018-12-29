4c51ca91cf1b19d12b238952f2a0bc65d8820fff
Hliana <liana.niehrah@gmail.com>
2018-12-29 10:29:15 -0800

    feature 01 complete

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 'use strict';
 
-//Application Dependencies
+// //Application Dependencies
+// const express = require('express');
+// const pg = require('pg');
+// const cors = require('cors');
+// const superAgent = require('superagent');
+
+// //Load env vars;
+// require('dotenv').config();
+
+// const PORT = process.env.PORT;
+
 const express = require('express');
-const pg = require('pg');
-const cors = require('cors');
-const superAgent = require('superagent');
 
-//Load env vars;
-require('dotenv').config();
+const superagent = require('superagent');
 
-const PORT = process.env.PORT;
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', home);
+
+function home(request, response){
+  response.render('pages/index');
+}
+
+app.listen(PORT, () => console.log(`APP is up on PORT : ${PORT}`));
