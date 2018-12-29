b2de36dcd199fb86e29b10cc62c0eb1fe8c9bd3d
Hliana <liana.niehrah@gmail.com>
2018-12-29 10:49:37 -0800

    search form complete

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 'use strict';
 
-// //Application Dependencies
-// const express = require('express');
-// const pg = require('pg');
-// const cors = require('cors');
-// const superAgent = require('superagent');
-
-// //Load env vars;
-// require('dotenv').config();
-
-// const PORT = process.env.PORT;
+//Application Dependencies
 
 const express = require('express');
 
@@ -25,8 +16,32 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', home);
 
-function home(request, response){
-  response.render('pages/index');
+app.post('/searches', search);
+
+function home(req, res){
+  res.render('pages/index');
 }
+
+function search(req, res){
+  const searchStr = req.body.search[0];
+  const searchType = req.body.search[1];
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  if (searchType === 'title') url+= `+intitle:${searchStr}`;
+  else if (searchType === 'author') url+= `+inauthor:$${searchStr}`;
+
+  return superagent.get(url)
+    .then(result => {
+      let books = result.body.items.map(book => new Book(book));
+      res.render('pages/show', {books});
+    })
+}
+
+//Constructor Functions
+function Book(book){
+  this.title = book.volumeInfo.title || 'Book Title does not exist';
+  this.placeholderImage = 'https://i.imgur.com/J5LVHEL.jpeg';
+}
+
 
 app.listen(PORT, () => console.log(`APP is up on PORT : ${PORT}`));
