d338a799eb6802ea2fb5584ba72f9c8e55b71d8c
Richard Bellamy <richard.bellamy123@gmail.com>
2018-12-29 10:50:47 -0800

    saves

--- server.js
+++ server.js
@@ -2,11 +2,31 @@
 
 //Application Dependencies
 const express = require('express');
-const pg = require('pg');
-const cors = require('cors');
-const superAgent = require('superagent');
+// const pg = require('pg');
+// const cors = require('cors');
+// const superAgent = require('superagent');
 
 //Load env vars;
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+//------------------------------------------------
+app.get('/', test);
+
+function test(req, res){
+    res.render('pages/index').catch(err => {
+        console.log(err);
+    });
+}
+
+
+
+app.listen(PORT, () => {
+    console.log(`app is up on port : ${PORT}`);
+  });
